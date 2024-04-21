1cf9b16dba7a554156e6efe010ccb55f23074125
adamaho <aho.adam01@gmail.com>
2024-04-20 22:06:26 -0400

    adding colors

--- .prettierrc.cjs
+++ .prettierrc.cjs
@@ -1,6 +1,6 @@
 module.exports = {
   plugins: ["@trivago/prettier-plugin-sort-imports"],
-  importOrder: ["^react", "<THIRD_PARTY_MODULES>", "\.css$"],
+  importOrder: ["^react", "<THIRD_PARTY_MODULES>", ".css$"],
   importOrderSeparation: true,
   importOrderSortSpecifiers: true,
   overrides: [{ files: ["*.ts"], options: { parser: "babel-ts" } }],
