012c3bcec83f12d3eadde8d49da77bce187c0896
adamaho <aho.adam01@gmail.com>
2024-05-30 21:42:18 -0400

    feat(apps-docs): split out layout components

--- apps/docs/astro.config.mjs
+++ apps/docs/astro.config.mjs
@@ -1,8 +1,10 @@
 import mdx from "@astrojs/mdx";
 import { defineConfig } from "astro/config";
 
+import react from "@astrojs/react";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://example.com",
-  integrations: [mdx()],
+  integrations: [mdx(), react()]
 });
