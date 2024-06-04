f65be1e18160932d7e15449fe0b4b74238239035
adamaho <aho.adam01@gmail.com>
2024-06-04 19:11:27 -0400

    feat(apps-docs): add mask-image on scroll area in sidebar

--- apps/docs/src/layouts/docs/sidebar/components/navigation/navigation.tsx
+++ apps/docs/src/layouts/docs/sidebar/components/navigation/navigation.tsx
@@ -5,20 +5,25 @@ import {
   ScrollAreaViewport,
 } from "@radix-ui/react-scroll-area";
 
+import "./navigation.css";
+
 export function Navigation() {
   return (
-    <div className="relative -mr-4 flex h-full flex-col overflow-hidden">
-      <ScrollArea className="min-h-0 grow overflow-hidden">
-        <ScrollAreaViewport className="h-full w-full">
-          <div className="h-[1000px] bg-transparent">Hello world</div>
-        </ScrollAreaViewport>
-        <ScrollAreaScrollbar
-          orientation="vertical"
-          className="hover:bg-black-a5 flex touch-none bg-transparent p-1 select-none data-[orientation=vertical]:w-[10px]"
-        >
-          <ScrollAreaThumb className="bg-grey-6 rounded-round relative flex-1 before:absolute before:top-1/2 before:left-1/2 before:h-full before:min-h-[44px] before:w-full before:min-w-[44px] before:-translate-x-1/2 before:-translate-y-1/2 before:content-['']" />
-        </ScrollAreaScrollbar>
-      </ScrollArea>
-    </div>
+    <ScrollArea
+      className="relative -mr-4 min-h-0 grow overflow-hidden"
+      type="always"
+    >
+      <ScrollAreaViewport className="wui-scroll-area-mask h-full w-full overscroll-contain scroll-auto">
+        <div className="bg-red-9 h-[1000px] py-[var(--wui-scroll-padding)]">
+          qweqweqwe
+        </div>
+      </ScrollAreaViewport>
+      <ScrollAreaScrollbar
+        orientation="vertical"
+        className="flex touch-none bg-transparent p-1 select-none data-[orientation=vertical]:w-[10px]"
+      >
+        <ScrollAreaThumb className="bg-grey-6 rounded-round relative flex-1 before:absolute before:top-1/2 before:left-1/2 before:h-full before:min-h-[44px] before:w-full before:min-w-[44px] before:-translate-x-1/2 before:-translate-y-1/2 before:content-['']" />
+      </ScrollAreaScrollbar>
+    </ScrollArea>
   );
 }
