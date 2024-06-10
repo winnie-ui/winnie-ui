72ab6f6d81da4901cde97e2827abb8790e457483
adamaho <aho.adam01@gmail.com>
2024-06-10 16:34:02 -0400

    feat(apps-docs): adds "what's next" cards to help guide reader to learn more

--- apps/docs/src/content/css/start/_components/whats-next-card-grid.tsx
+++
@@ -1,7 +0,0 @@
-import type { PropsWithChildren } from "react";
-
-import "./whats-next-card-grid.css";
-
-export function WhatsNextCardGrid({ children }: PropsWithChildren) {
-  return <div className="whats-next-card-grid">{children}</div>;
-}
