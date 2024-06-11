5f9b3c25b3b16c54e58f75990ee16ac6f1101cb2
adamaho <aho.adam01@gmail.com>
2024-06-11 11:44:47 -0400

    feat(apps-docs): adds styled inline code and polishes headings

--- apps/docs/astro.config.mjs
+++ apps/docs/astro.config.mjs
@@ -1,7 +1,9 @@
 import mdx from "@astrojs/mdx";
 import react from "@astrojs/react";
 import expressiveCode from "astro-expressive-code";
+import { addClassName } from 'astro-expressive-code/hast';
 import { defineConfig } from "astro/config";
+
 // import fs from "node:fs";
 //
 // const jsoncString = fs.readFileSync(
@@ -18,9 +20,20 @@ export default defineConfig({
       frames: {
         showCopyToClipboardButton: false,
       },
+      plugins: [
+        {
+          // Adds not-content className to rendered expressiveCode block
+          name: "Not Content",
+          hooks: {
+            postprocessRenderedBlock: ({ renderData }) => {
+              addClassName(renderData.blockAst, "not-content");
+            },
+          },
+        },
+      ],
       themes: ["github-light"],
       themeCssSelector: (theme) => `[data-code-theme="${theme.name}"]`,
-      useDarkModeMediaQuery: false
+      useDarkModeMediaQuery: false,
     }),
     mdx(),
     react(),
