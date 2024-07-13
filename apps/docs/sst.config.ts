4b998230d6f13ffa0a48662b609d7b00ad847a9d
adamaho <aho.adam01@gmail.com>
2024-07-13 19:22:11 -0400

    feat(apps-docs): configuring cloudflare dns with winnie-ui.com domain

--- apps/docs/sst.config.ts
+++ apps/docs/sst.config.ts
@@ -1,14 +1,19 @@
 /// <reference path="./.sst/platform/config.d.ts" />
-
 export default $config({
   app(input) {
     return {
       name: "winnie-docs",
       removal: input?.stage === "production" ? "retain" : "remove",
       home: "aws",
+      providers: { cloudflare: true },
     };
   },
   async run() {
-    new sst.aws.Astro("WinnieDocs");
+    new sst.aws.Astro("WinnieDocs", {
+      domain: {
+        name: "winnie-ui.com",
+        dns: sst.cloudflare.dns(),
+      },
+    });
   },
 });
