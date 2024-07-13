530d278fdf970931b308275eadf3df7fec378c1a
adamaho <aho.adam01@gmail.com>
2024-07-13 19:23:26 -0400

    feat(apps-docs): configuring site in astro config

--- apps/docs/astro.config.mjs
+++ apps/docs/astro.config.mjs
@@ -6,7 +6,7 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://example.com",
+  site: "https://winnie-ui.com",
   output: "server",
   adapter: aws(),
   redirects: {
