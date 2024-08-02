26e59ff5c402d571983e552735bd750b38a6cfc4
adamaho <aho.adam01@gmail.com>
2024-08-01 21:23:53 -0400

    feat(apps-docs): updates deps and uses radix for theme picker dialog to fix event propagation bug

--- apps/docs/src/layouts/docs/sidebar/components/theme-picker/theme-picker-dialog.tsx
+++ apps/docs/src/layouts/docs/sidebar/components/theme-picker/theme-picker-dialog.tsx
@@ -1,12 +1,13 @@
 import {
-  Button,
   Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogOverlay,
+  DialogPortal,
+  DialogTitle,
   DialogTrigger,
-  Heading,
-  Modal,
-  ModalOverlay,
-} from "react-aria-components";
-
+} from "@radix-ui/react-dialog";
 import { Settings2, X } from "lucide-react";
 
 import { ThemePicker } from "./theme-picker";
@@ -18,43 +19,33 @@ import "./theme-picker-dialog.css";
  * -----------------------------------------------------------------------------------------------*/
 function ThemePickerDialog() {
   return (
-    <DialogTrigger>
-      <Button
+    <Dialog>
+      <DialogTrigger
         className="theme-picker-modal-trigger"
         aria-label="Customize Theme"
       >
         <Settings2 />
-      </Button>
-      <ModalOverlay className="theme-picker-modal-overlay" isDismissable>
-        <Modal isDismissable className="theme-picker-modal wui-scrollbar">
-          <Dialog className="theme-picker-dialog">
-            {({ close }) => {
-              return (
-                <>
-                  <div className="theme-picker-dialog-header">
-                    <Heading className="theme-picker-dialog-title" slot="title">
-                      Customize Theme
-                    </Heading>
-                    <p className="theme-picker-dialog-description">
-                      Change the vibes of your docs
-                    </p>
-                  </div>
-                  <div className="theme-picker-dialog-content">
-                    <ThemePicker />
-                  </div>
-                  <Button
-                    onPressStart={close}
-                    className="theme-picker-dialog-close"
-                  >
-                    <X />
-                  </Button>
-                </>
-              );
-            }}
-          </Dialog>
-        </Modal>
-      </ModalOverlay>
-    </DialogTrigger>
+      </DialogTrigger>
+      <DialogPortal>
+        <DialogOverlay className="theme-picker-modal-overlay" />
+        <DialogContent className="theme-picker-modal wui-scrollbar">
+          <div className="theme-picker-dialog-header">
+            <DialogTitle className="theme-picker-dialog-title" slot="title">
+              Customize Theme
+            </DialogTitle>
+            <DialogDescription className="theme-picker-dialog-description">
+              Change the vibes of your docs
+            </DialogDescription>
+          </div>
+          <div className="theme-picker-dialog-content">
+            <ThemePicker />
+          </div>
+          <DialogClose className="theme-picker-dialog-close">
+            <X />
+          </DialogClose>
+        </DialogContent>
+      </DialogPortal>
+    </Dialog>
   );
 }
 
