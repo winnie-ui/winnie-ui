6a4bd2683e65d4ec8703072f6defcd75e9903586
adamaho <aho.adam01@gmail.com>
2024-10-20 18:01:13 -0400

    feat: configure tanstack router for showcase application

--- apps/docs/astro.config.mjs
+++ apps/docs/astro.config.mjs
@@ -1,18 +1,28 @@
 import mdx from "@astrojs/mdx";
 import react from "@astrojs/react";
+import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 import expressiveCode from "astro-expressive-code";
 import aws from "astro-sst";
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://winnie-ui.com",
-	output: "server",
-	adapter: aws(),
-	redirects: {
-		"": "/css",
-		"/": "/css",
-		"/css/docs": "/css/docs/start/quick-start",
-	},
-	integrations: [expressiveCode(), mdx(), react()],
+  site: "https://winnie-ui.com",
+  output: "server",
+  adapter: aws(),
+  redirects: {
+    "": "/css",
+    "/": "/css",
+    "/css/docs": "/css/docs/start/quick-start",
+  },
+  integrations: [expressiveCode(), mdx(), react()],
+  vite: {
+    plugins: [
+      TanStackRouterVite({
+        routesDirectory: "./src/showcase/routes",
+        generatedRouteTree: "./src/showcase/routeTree.gen.ts",
+        quoteStyle: "double",
+      }),
+    ],
+  },
 });
