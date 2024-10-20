9bc812ebd3f5a7aa90d84d43c642053ac7f5a0fe
adamaho <aho.adam01@gmail.com>
2024-10-20 18:04:27 -0400

    feat: configure tanstack router for showcase application (#60)
    * feat: configure tanstack router for showcase application
    
    * fix: sst dev script

---
+++ apps/docs/src/showcase/main.tsx
@@ -0,0 +1,21 @@
+import { RouterProvider, createRouter } from "@tanstack/react-router";
+import { StrictMode } from "react";
+
+// Import the generated route tree
+import { routeTree } from "./routeTree.gen";
+
+// Create a new router instance
+const router = createRouter({ routeTree });
+
+// Register the router instance for type safety
+declare module "@tanstack/react-router" {
+  interface Register {
+    router: typeof router;
+  }
+}
+
+export const Showcase = () => (
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>
+);
---
+++ apps/docs/src/showcase/routes/__root.tsx
@@ -0,0 +1,20 @@
+import { Link, Outlet, createRootRoute } from "@tanstack/react-router";
+import { TanStackRouterDevtools } from "@tanstack/router-devtools";
+
+export const Route = createRootRoute({
+  component: () => (
+    <>
+      <div className="p-2 flex gap-2">
+        <Link to="/showcase" className="[&.active]:font-bold">
+          Home
+        </Link>{" "}
+        <Link to="/showcase/about" className="[&.active]:font-bold">
+          About
+        </Link>
+      </div>
+      <hr />
+      <Outlet />
+      <TanStackRouterDevtools />
+    </>
+  ),
+});
