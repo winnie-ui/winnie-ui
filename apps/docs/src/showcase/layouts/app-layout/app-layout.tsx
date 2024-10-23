29ba79cb5e309855871851459164a42177fc05b9
adamaho <aho.adam01@gmail.com>
2024-10-22 20:36:10 -0400

    feat: safe area triangle

--- apps/docs/src/showcase/layouts/app-layout/app-layout.tsx
+++ apps/docs/src/showcase/layouts/app-layout/app-layout.tsx
@@ -32,6 +32,115 @@ import "./app-layout.css";
 //     </AriaDisclosure>
 //   );
 // }
+//
+/* -------------------------------------------------------------------------------------------------
+ * Utilities
+ * -----------------------------------------------------------------------------------------------*/
+/**
+ * Computes the area of a triangle given a set of 3 coordinates
+ *
+ * @param points all of the x, y coordinates of the triangle
+ */
+function getAreaOfTriangle({
+  p1,
+  p2,
+  p3,
+}: {
+  p1: {
+    x: number;
+    y: number;
+  };
+  p2: {
+    x: number;
+    y: number;
+  };
+  p3: {
+    x: number;
+    y: number;
+  };
+}) {
+  return Math.abs(
+    (p1.x * (p2.y - p3.y) + p2.x * (p3.y - p1.y) + p3.x * (p1.y - p2.y)) / 2.0,
+  );
+}
+
+/**
+ * Computes the area of a triangle given a set of 3 coordinates
+ *
+ * @param points all of the x, y coordinates of the triangle
+ */
+function isCursorSafe(
+  mouseX: number,
+  mouseY: number,
+  {
+    p1,
+    p2,
+    p3,
+  }: {
+    p1: {
+      x: number;
+      y: number;
+    };
+    p2: {
+      x: number;
+      y: number;
+    };
+    p3: {
+      x: number;
+      y: number;
+    };
+  },
+) {
+  // Calculates area of reference triangle
+  const areaABC = getAreaOfTriangle({ p1, p2, p3 });
+
+  // Calculate the areas of the triangles formed by the mouse position and two vertices of the triangle
+  const areaPAB = getAreaOfTriangle({
+    p1: {
+      x: mouseX,
+      y: mouseY,
+    },
+    p2: {
+      x: p1.x,
+      y: p1.y,
+    },
+    p3: {
+      x: p2.x,
+      y: p2.y,
+    },
+  });
+  const areaPBC = getAreaOfTriangle({
+    p1: {
+      x: mouseX,
+      y: mouseY,
+    },
+    p2: {
+      x: p2.x,
+      y: p2.y,
+    },
+    p3: {
+      x: p3.x,
+      y: p3.y,
+    },
+  });
+  const areaPCA = getAreaOfTriangle({
+    p1: {
+      x: mouseX,
+      y: mouseY,
+    },
+    p2: {
+      x: p3.x,
+      y: p3.y,
+    },
+    p3: {
+      x: p1.x,
+      y: p1.y,
+    },
+  });
+
+  // Check if the sum of the smaller triangle areas equals the area of the original triangle
+  return areaABC === areaPAB + areaPBC + areaPCA;
+}
 
 /* -------------------------------------------------------------------------------------------------
  * AppLayout
@@ -39,6 +148,7 @@ import "./app-layout.css";
 function AppLayout({ children }: PropsWithChildren) {
   const sidebarRef = useRef<HTMLElement | null>(null);
   const buttonRef = useRef<HTMLDivElement | null>(null);
+  const timerRef = useRef<any | null>(null);
 
   const { hoverProps } = useHover({
     onHoverStart: () => {
@@ -57,7 +167,29 @@ function AppLayout({ children }: PropsWithChildren) {
   });
 
   useEffect(() => {
-    function handleMouseOver(e: MouseEvent) {
+    function handleMouseMove(e: MouseEvent) {
+      const mouseX = e.clientX;
+      const mouseY = e.clientY;
+
+      // Get the coordinates of the parent and target elements
+      const parentRect = buttonRef?.current?.getBoundingClientRect();
+      const targetRect = sidebarRef?.current?.getBoundingClientRect();
+
+      const p1 = {
+        x: parentRect?.right,
+        y: parentRect?.top,
+      };
+
+      const p2 = {
+        x: targetRect?.left,
+        y: targetRect?.top,
+      };
+
+      const p3 = {
+        x: targetRect?.right,
+        y: targetRect?.top,
+      };
+
       const breakpoint = window.matchMedia("(min-width: 768px)");
 
       if (!breakpoint.matches) {
@@ -80,13 +212,16 @@ function AppLayout({ children }: PropsWithChildren) {
       }
 
       // if the current mouse position is within the safe triangle
-      // TODO: calculate safe area
+      if (isCursorSafe(mouseX, mouseY, { p1, p2, p3 })) {
+        return;
+      }
 
       // if the current target is within the button of the sidebar
       if (
         buttonRef.current?.contains(e.target as Node) ||
         sidebarRef.current?.contains(e.target as Node)
       ) {
+        clearTimeout(timerRef.current);
         return;
       }
 
@@ -104,14 +239,20 @@ function AppLayout({ children }: PropsWithChildren) {
         return;
       }
 
-      sidebarRef.current.setAttribute("data-state", "closed");
+      if (timerRef.current) {
+        clearTimeout(timerRef.current);
+      }
+
+      timerRef.current = setTimeout(() => {
+        sidebarRef?.current?.setAttribute("data-state", "closed");
+      }, 1000);
     }
 
-    document.addEventListener("mousemove", handleMouseOver);
+    document.addEventListener("mousemove", handleMouseMove);
     document.addEventListener("mouseleave", handleMouseLeave);
 
     return () => {
-      document.removeEventListener("mousemove", handleMouseOver);
+      document.removeEventListener("mousemove", handleMouseMove);
       document.removeEventListener("mouseleave", handleMouseLeave);
     };
   }, []);
