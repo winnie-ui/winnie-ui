673aa074772297a940d7b515ea010b6b60985dc6
adamaho <aho.adam01@gmail.com>
2024-12-08 20:27:27 -0500

    making the sidebar and search work across collections

--- apps/docs/src/layouts/docs/sidebar/components/search.tsx
+++ apps/docs/src/layouts/docs/sidebar/components/search.tsx
@@ -1,11 +1,16 @@
+import type { CollectionKey } from "astro:content";
 import { SearchCommandMenu } from "~/components/search-command-menu/search-command-menu";
 import { useMediaQuery } from "~/hooks/use-media-query";
 
-export const Search = () => {
+type SearchProps = {
+  collection: CollectionKey;
+};
+
+export const Search = ({ collection }: SearchProps) => {
   /**
    * Computes if the current breakpoint is less than 768px
    */
   const isMobile = useMediaQuery("(max-width: 768px)");
 
-  return !isMobile ? <SearchCommandMenu /> : null;
+  return !isMobile ? <SearchCommandMenu collection={collection} /> : null;
 };
