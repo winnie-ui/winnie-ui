060e84a938c8a2d56217c9af3d044f66b44e256b
adamaho <aho.adam01@gmail.com>
2024-12-21 14:51:07 -0500

    fix: remove query debtools

--- apps/showcase/src/main.tsx
+++ apps/showcase/src/main.tsx
@@ -1,11 +1,11 @@
 import { RouterProvider, createRouter } from "@tanstack/react-router";
 import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 
 import { ZeroProvider } from "@rocicorp/zero/react";
 
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-import { createRoot } from "react-dom/client";
 import { routeTree } from "./routeTree.gen";
+import { zero } from "./singletons/zero";
 
 import "@fontsource/inter/400.css";
 import "@fontsource/inter/500.css";
@@ -13,18 +13,12 @@ import "@fontsource/inter/600.css";
 import "@fontsource/inter/700.css";
 import "@fontsource/inter/800.css";
 import "./main.css";
-import { zero } from "./singletons/zero";
 
 // Create a new router instance
 const router = createRouter({
   routeTree,
   Wrap: ({ children }) => {
-    return (
-      <ZeroProvider zero={zero}>
-        {children}
-        <ReactQueryDevtools initialIsOpen={false} />
-      </ZeroProvider>
-    );
+    return <ZeroProvider zero={zero}>{children}</ZeroProvider>;
   },
 });
 
